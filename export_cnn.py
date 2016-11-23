805d05b06e0b6b839bb22bdd967a7b0d1ab70ba6
tommy <tvomxq@yahoo.co.jp>
2016-11-23 19:09:01 +0900

    cnn in cpp
    no pulling layer

---
+++ export_cnn.py
@@ -0,0 +1,227 @@
+
+from __future__ import print_function
+import struct
+import numpy as np
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import training, Chain, Variable
+from chainer.training import extensions
+import argparse
+import glob
+
+import sys
+
+class Model(Chain):
+    insize=4
+    def __init__(self, n_out):
+        super(Model, self).__init__(
+            conv1 = L.Convolution2D(None, 64, 3, pad=1), 
+            conv2 = L.Convolution2D(None, 128, 3, pad=1),
+            conv3 = L.Convolution2D(None, 256, 3, pad=1),
+            fc4 = L.Linear(None, 4096),
+            fc5 = L.Linear(None, 1024),
+            fc6 = L.Linear(None, n_out),
+            )
+        self.train = True
+
+    def __call__(self, x):
+        h = F.relu(self.conv1(x))
+        h = F.relu(self.conv2(h))
+        h = F.relu(self.conv3(h))
+        h = F.relu(self.fc4(h))
+        h = F.relu(self.fc5(h))
+        h = self.fc6(h)
+        return h
+
+def load_test_data(input_size, data_path):
+    input_num = (input_size+1)*(input_size+1)
+    forpred_data = []
+    forpred_label = []
+    data_list = glob.glob(data_path + "*")
+    for datafileName in data_list:
+        print("open ", datafileName)
+        data_raw = open(datafileName)
+        i = 0
+        for line in data_raw:
+            i = i + 1
+            lineData = np.asarray([np.float32(int(x)/1023.0) for x in line.split(",")[0:input_num]])
+            reshapedData = lineData.reshape(input_size+1,input_size+1)
+            label = np.int32(line.split(",")[input_num])
+            forpred_data.append(np.asarray([reshapedData]))
+            forpred_label.append(label)
+            if i > 9:
+                break
+
+    return forpred_data, forpred_label
+
+parse = argparse.ArgumentParser(description="to use on cpp")
+parse.add_argument('--filtersize','-f',type=int, default=3)
+parse.add_argument('--padding','-p', type=int, default=1)
+parse.add_argument('--cusize','-s',type=int, default=4)
+parse.add_argument('--path','-pa', default="./valueDatas_of_wh4_kimono.txt")
+parse.add_argument('--channels','-c', type=int, default=1)
+parse.add_argument('--conv1out','-c1', type=int, default=64)
+parse.add_argument('--conv2out','-c2', type=int, default=128)
+parse.add_argument('--conv3out','-c3', type=int, default=256)
+parse.add_argument('--fc4','-f4', type=int, default=4096)
+parse.add_argument('--fc5','-f5', type=int, default=1024)
+parse.add_argument('--fc6','-f6', type=int, default=35)
+
+args = parse.parse_args()
+
+filter_size = args.filtersize
+padding = args.padding
+cusize = args.cusize
+channels = args.channels
+c1out = args.conv1out
+c2out = args.conv2out
+c3out = args.conv3out
+f4out = args.fc4
+f5out = args.fc5
+f6out = args.fc6
+
+
+model = L.Classifier(Model(35))
+chainer.serializers.load_npz('./my_s4_e100.model', model)
+
+d = bytearray()
+l = bytearray()
+
+c0 = len(model.predictor.conv1.W.data)
+c1 = len(model.predictor.conv1.W.data[0])
+c2 = len(model.predictor.conv1.W.data[0][0])
+c3 = len(model.predictor.conv1.W.data[0][0][0])
+b0 = len(model.predictor.conv1.b.data)
+print(c0,c1,c2,c3,b0)
+print("W[0] : ", model.predictor.conv1.W.data[0][0][0][0])
+print("W[1*64*3*3-1] : ", model.predictor.conv1.W.data[63][0][2][2])
+print("b[0] : ", model.predictor.conv1.b.data[0])
+print("b[64-1] : ", model.predictor.conv1.b.data[63])
+for v in model.predictor.conv1.W.data.reshape(c0*c1*c2*c3):
+    d += struct.pack('f',v)
+for v in model.predictor.conv1.b.data:
+    d += struct.pack('f',v)
+
+    
+c0 = len(model.predictor.conv2.W.data)
+c1 = len(model.predictor.conv2.W.data[0])
+c2 = len(model.predictor.conv2.W.data[0][0])
+c3 = len(model.predictor.conv2.W.data[0][0][0])
+b0 = len(model.predictor.conv2.b.data)
+print(c0,c1,c2,c3,b0)
+print("W2[0] : ", model.predictor.conv2.W.data[0][0][0][0])
+print("W2[64*128*3*3-1] : ", model.predictor.conv2.W.data[127][63][2][2])
+print("b2[0] : ", model.predictor.conv2.b.data[0])
+print("b2[128-1] : ", model.predictor.conv2.b.data[127])
+for v in model.predictor.conv2.W.data.reshape(c0*c1*c2*c3):
+    d += struct.pack('f',v)
+for v in model.predictor.conv2.b.data:
+    d += struct.pack('f',v)
+    
+
+c0 = len(model.predictor.conv3.W.data)
+c1 = len(model.predictor.conv3.W.data[0])
+c2 = len(model.predictor.conv3.W.data[0][0])
+c3 = len(model.predictor.conv3.W.data[0][0][0])
+b0 = len(model.predictor.conv3.b.data)
+print(c0,c1,c2,c3,b0)
+print("W3[0] : ", model.predictor.conv3.W.data[0][0][0][0])
+print("W3[128*256*3*3-1] : ", model.predictor.conv3.W.data[255][127][2][2])
+print("b3[0] : ", model.predictor.conv3.b.data[0])
+print("b3[256-1] : ", model.predictor.conv3.b.data[255])
+print(c0,c1,c2,c3,b0)
+for v in model.predictor.conv3.W.data.reshape(c0*c1*c2*c3):
+    d += struct.pack('f',v)
+for v in model.predictor.conv3.b.data:
+    d += struct.pack('f',v)
+    
+
+l0 = len(model.predictor.fc4.W.data)
+l1 = len(model.predictor.fc4.W.data[0])
+b0 = len(model.predictor.fc4.b.data)
+print(l0,l1,b0)
+for v in model.predictor.fc4.W.data.reshape(l0*l1):
+    d += struct.pack('f',v)
+    l += struct.pack('f',v)
+for v in model.predictor.fc4.b.data:
+    d += struct.pack('f',v)
+    l += struct.pack('f',v)
+
+l0 = len(model.predictor.fc5.W.data)
+l1 = len(model.predictor.fc5.W.data[0])
+b0 = len(model.predictor.fc5.b.data)
+print(l0,l1,b0)
+for v in model.predictor.fc5.W.data.reshape(l0*l1):
+    d += struct.pack('f',v)
+    l += struct.pack('f',v)
+for v in model.predictor.fc5.b.data:
+    d += struct.pack('f',v)
+    l += struct.pack('f',v)
+
+
+l0 = len(model.predictor.fc6.W.data)
+l1 = len(model.predictor.fc6.W.data[0])
+b0 = len(model.predictor.fc6.b.data)
+print(l0,l1,b0)
+for v in model.predictor.fc6.W.data.reshape(l0*l1):
+    d += struct.pack('f',v)
+    l += struct.pack('f',v)
+for v in model.predictor.fc6.b.data:
+    d += struct.pack('f',v)
+    l += struct.pack('f',v)
+    
+
+open("and.dat","w").write(d);
+open("linearonly.dat","w").write(l);
+
+testDatas, correctLabels = load_test_data(args.cusize, args.path)
+
+x = np.asarray(testDatas)
+y = model.predictor(x).data
+for i in np.arange(len(y)):
+    print(i,x[i])
+    print("  ---", np.argmax(y[i]),"---")
+    print("  ",y[i])
+
+print("---equal---")
+#for yx in x[0][0]:
+#    print(yx)
+#print("done inimg")
+#for yx in model.predictor.conv1.W.data[0][0]:
+#    print(yx)
+#print("done filter")
+
+h = F.relu(model.predictor.conv1(x))
+#print(len(h.data))
+#print(len(h.data[0]))
+#print(len(h.data[0][0]))
+#print(len(h.data[0][0][0]))
+#for a in h.data[0][0]:
+#    print(a.reshape(-1))
+
+h = F.relu(model.predictor.conv2(h.data))
+h = F.relu(model.predictor.conv3(h.data))
+
+bb = bytearray()
+for v in h.data.reshape(len(h.data), -1):
+    for v2 in v:
+        bb += struct.pack('f',v2)
+#print(len(h.data),len(h.data[0]),len(h.data[0][0]),len(h.data[0][0][0]))
+open("conved.dat","w").write(bb);
+
+h = F.relu(model.predictor.fc4(h.data))
+#print(len(h.data))
+#print(len(h.data[0]))
+bbb = bytearray()
+for v in h.data.reshape(len(h.data)*len(h.data[0])):
+    bbb += struct.pack('f',v)
+open("lineared.dat","w").write(bbb)
+
+h = F.relu(model.predictor.fc5(h.data))
+h = model.predictor.fc6(h.data)
+#for i in np.arange(len(h.data)):
+#    print(i,x[i])
+#    print("  ---", np.argmax(h.data[i]),"---")
+#    print("  ",h.data[i])
+    
---
+++ import_cnn.cpp
@@ -0,0 +1,243 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <string>
+#include <sstream>
+
+
+typedef std::vector<float> vf;
+
+class Linear {
+private:
+    vf W;
+    vf b;
+    const int n_in, n_out;
+    float relu(float x) {
+        return (x > 0) ? x : 0;
+    }
+public:
+    // Linearの方のコンストラクタ
+    Linear(int in, int out) : n_in(in), n_out(out) {
+        W.resize(n_in * n_out);
+        b.resize(n_out);
+    }
+    void read(std::ifstream &ifs) {
+        ifs.read((char*)W.data(), sizeof(float)*n_in * n_out);
+        ifs.read((char*)b.data(), sizeof(float)*n_out);
+    }
+    vf get(vf x) {
+        vf y(n_out);
+        for (int i = 0; i < n_out; i++) {
+            y[i] = 0.0;
+            for (int j = 0; j < n_in; j++) {
+                y[i] += W[i * n_in + j] * x[j];
+            }
+            y[i] += b[i];
+        }
+        return y;
+    }
+    vf get_relu(vf x){
+        vf y = get(x);
+        for (int i=0; i<n_out; i++) {
+            y[i] = relu(y[i]);
+        }
+        return y;
+    }
+};
+
+class Convolution2D {
+private:
+    vf C;
+    vf b;
+    const int n_in_channels, n_out_filter_num, n_filter_size, n_padding, n_stride;
+    float relu(float x) {
+        return (x > 0) ? x : 0;
+    }
+public:
+    // Convの方のコンストラクタ
+    Convolution2D(int infn, int outfn, int nfs, int np, int ns) : n_in_channels(infn), n_out_filter_num(outfn), n_filter_size(nfs), n_padding(np), n_stride(ns){
+        C.resize(n_in_channels * n_out_filter_num * n_filter_size * n_filter_size);
+        b.resize(n_out_filter_num);
+    }
+    void read(std::ifstream &ifs) {
+        ifs.read((char*)C.data(), sizeof(float)*n_in_channels*n_out_filter_num*n_filter_size*n_filter_size);
+        ifs.read((char*)b.data(), sizeof(float)*n_out_filter_num);
+    }
+    std::vector< vf > get(std::vector< vf > x) {
+       
+        int in_size = sqrt(x[0].size());
+        int out_size = ((in_size+n_padding*2-n_filter_size)/n_stride+1);
+        int half_of_filsize = n_filter_size >> 1;
+        // paddingを考慮した入力を作る:　端は0
+        std::vector< vf > tempX1( x.size() ,vf((in_size+n_padding*2)*(in_size+n_padding*2), 0));
+        
+        int c = 0;
+        for (auto cx: x){
+            for (int y=1; y<in_size+1; y++) {
+                for (int x=1; x<in_size+1; x++) {
+                    tempX1[c][y*(in_size+n_padding*2)+x] = cx[(y-1)*in_size+(x-1)];
+                }
+            }
+            c++;
+        }
+        
+        // 出力のベクトル
+        std::vector< vf > y ( n_out_filter_num, vf( out_size*out_size ) );
+        // フィルタごとのfor
+        for (int fil_i=0; fil_i<n_out_filter_num; fil_i++) {
+            // 出力画像のfor
+            for (int in_y=n_padding; in_y<in_size+n_padding*2-1; in_y+=n_stride) {
+                for (int in_x=n_padding; in_x<in_size+n_padding*2-1; in_x+=n_stride) {
+                    y[fil_i][(in_y-n_padding)*out_size+(in_x-n_padding)] = 0;
+                    // チャンネルのfor
+                    for (int c=0; c<n_in_channels; c++) {
+                        // フィルタ内のfor：畳込み
+                        for (int k=0; k<n_filter_size; k++) {
+                            for (int l=0; l<n_filter_size; l++) {
+                                float pixVal = tempX1[c][(in_y+k-half_of_filsize)*(in_size+n_padding*2)+(in_x+l-half_of_filsize)];
+                                float filVal = C[fil_i*(n_in_channels*n_filter_size*n_filter_size) +
+                                                c*(n_filter_size*n_filter_size) +
+                                                k*(n_filter_size) +
+                                                 l];
+                                y[fil_i][(in_y-n_padding)*out_size+(in_x-n_padding)] += pixVal * filVal;
+                            }
+                        }
+                    }
+                    // バイアスを足す
+                    y[fil_i][(in_y-n_padding)*out_size+(in_x-n_padding)] += b[fil_i];
+                }
+            }
+        }
+        return y;
+    }
+    std::vector< vf > relu(std::vector< vf > x) {
+        int j = 0;
+        for (auto x1 : x){
+            int i = 0;
+            for (auto x2:x1){
+                x[j][i] = relu(x2);
+                i++;
+            }
+            j++;
+        }
+        return x;
+    }
+};
+
+
+int argmax(vf &v) {
+    float max = v[0];
+    int max_i = 0;
+    for (int i = 1; i < v.size(); i++) {
+        if (max < v[i]) {
+            max_i = i;
+            max = v[i];
+        }
+    }
+    return max_i;
+}
+std::vector<std::string> split(const std::string &str, char sep)
+{
+    std::vector<std::string> v;
+    std::stringstream ss(str);
+    std::string buffer;
+    while( std::getline(ss, buffer, sep) ) {
+        v.push_back(buffer);
+    }
+    return v;
+}
+std::vector< vf > load_test_data(int input_size){
+    int input_num = (input_size+1) * (input_size+1);
+    std::ifstream ifs("input_data.txt");
+    std::string str;
+    std::vector< vf > v (10, vf(25) );
+    char index_to_finish10 = 0;
+    if (ifs.fail())
+    {
+        std::cerr << "失敗" << std::endl;
+        return v;
+    }
+    while (getline(ifs, str))
+    {
+        std::vector<std::string> nums = split(str, ',');
+        int index_to_test = 0;
+        for (auto vo: nums){
+            v[index_to_finish10][index_to_test] = atoi( vo.c_str() );
+            index_to_test++;
+            if (index_to_test > 24) {
+                break;
+            }
+        }
+        index_to_finish10++;
+        if (index_to_finish10 > 9) {
+            break;
+        }
+    }
+    
+    return v;
+}
+
+int main(void) {
+    const int n_size = 128;
+    const int n_channels = 1;
+    const int n_in = 576;
+    const int n_units = 1024;
+    const int n_out = 35;
+    std::ifstream ifs("and.dat");
+    
+    std::cout << "set convolution" << std::endl;
+    // conv1の準備 -> 読み込み -> 畳込み -> 出力層数 -> conv2の準備...
+    Convolution2D conv1(n_channels, 64, 3, 1, 1); //in_channel,out_channel,filtersize,padding,stride
+    Convolution2D conv2(64, 128, 3, 1, 1);
+    Convolution2D conv3(128,256, 3, 1, 1);
+
+    std::cout << "read ifs" << std::endl;
+    conv1.read(ifs);
+    conv2.read(ifs);
+    conv3.read(ifs);
+
+    std::cout << "set linear" << std::endl;
+    Linear fc4(6400, 4096), fc5(4096, 1024), fc6(1024, 35);
+    fc4.read(ifs);
+    fc5.read(ifs);
+    fc6.read(ifs);
+    
+    std::vector< vf > test_datas = load_test_data(4);
+    
+
+    std::cout << "prediction" << std::endl;
+    // 全部のデータ
+    int tesi = 0;
+    for (auto test_data : test_datas){
+        // このループでpredictするためのデータを作る. 今回はchannelが1なので(1,25)の2次元
+        std::vector< vf > x2(1, vf(25) );
+        int i = 0;
+        for (auto v2:test_data){
+            x2[0][i] = v2/1023;
+            i++;
+        }
+        
+        std::vector< vf > h = conv3.relu(conv3.get(conv2.relu(conv2.get(conv1.relu(conv1.get(x2))))));
+        vf x3;
+        for(auto h1:h){
+            for(auto h2:h1){
+                x3.push_back(h2);
+            }
+        }
+        vf y = fc6.get(fc5.get_relu(fc4.get_relu(x3)));
+        // あってるか確認するとこー
+        std::cout << tesi <<" ";
+        std::cout << "["<<x2[0][0]<<" "<<x2[0][1]<<" "<<x2[0][2]<<", ... ,"<<x2[0][22]<<" "<<x2[0][23]<<" "<<x2[0][24]<<"]"<<std::endl;
+        std::cout << "  ---" << argmax(y) << "---" << std::endl;
+        for (auto yy:y){
+            std::cout << yy << " ";
+        }
+        std::cout << std::endl;
+        tesi++;
+    }
+}
+
+
